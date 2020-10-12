c593c4075d83819efc345412b5f0d3b933984c4b
Adam Fowler <adamfowler71@gmail.com>
2020-10-12 15:42:27 +0100

    Add first test, still not compiling
    Complaining about XCTest versions for some reason
    ld: warning: dylib (/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/XCTest.framework/XCTest) was built for newer macOS version (10.15) than being linked (10.10)

--- IntegrationTests/tests_04_performance/test_01_resources/test_awsrequest.swift
+++ IntegrationTests/tests_04_performance/test_01_resources/test_awsrequest.swift
@@ -11,16 +11,44 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+import Foundation
 import SotoCore
+import SotoTestUtils
 
 func run(identifier: String) {
-    struct A: Encodable {
-        let string: String
+    struct Input: AWSEncodableShape & Decodable {
+        let s: String
+        let i: [Int64]
     }
-    let a = A(string: "Test")
+    struct Output: AWSDecodableShape & Encodable {
+        let s: String
+        let i: Int
+    }
+    let awsServer = AWSTestServer(serviceProtocol: .json)
+    let config = createServiceConfig(serviceProtocol: .json(version: "1.1"), endpoint: awsServer.address)
+    let client = createAWSClient(credentialProvider: .empty, middlewares: [AWSLoggingMiddleware()])
+    defer {
+        try? client.syncShutdown()
+        try? awsServer.stop()
+    }
+    let input = Input(s: "second", i: [1, 2, 4, 8])
     measure(identifier: identifier) {
         for _ in 0..<1000 {
-            _ = try? QueryEncoder().encode(a)
+            do {
+                let response: EventLoopFuture<Output> = client.execute(operation: "test", path: "/", httpMethod: .POST, serviceConfig: config, input: input, logger: TestEnvironment.logger)
+
+                try awsServer.processRaw { request in
+                    let receivedInput = try JSONDecoder().decode(Input.self, from: request.body)
+                    let output = Output(s: receivedInput.s, i: receivedInput.i.count)
+                    let byteBuffer = try JSONEncoder().encodeAsByteBuffer(output, allocator: ByteBufferAllocator())
+                    let response = AWSTestServer.Response(httpStatus: .ok, headers: [:], body: byteBuffer)
+                    return .result(response)
+                }
+
+                _ = try response.wait()
+            } catch {
+                
+            }
         }
         return 1000
     }
--- Tests/SotoCoreTests/AWSClientTests.swift
+++ Tests/SotoCoreTests/AWSClientTests.swift
@@ -131,6 +131,45 @@ class AWSClientTests: XCTestCase {
             XCTFail("Unexpected error: \(error)")
         }
     }
+    func run(identifier: String) {
+        struct Input: AWSEncodableShape & Decodable {
+            let s: String
+            let i: [Int64]
+        }
+        struct Output: AWSDecodableShape & Encodable {
+            let s: String
+            let i: Int
+        }
+        let awsServer = AWSTestServer(serviceProtocol: .json)
+        let config = createServiceConfig(serviceProtocol: .json(version: "1.1"), endpoint: awsServer.address)
+        let client = createAWSClient(credentialProvider: .empty, middlewares: [AWSLoggingMiddleware()])
+        defer {
+            try? client.syncShutdown()
+            try? awsServer.stop()
+        }
+        let input = Input(s: "second", i: [1, 2, 4, 8])
+
+        measure {
+            for _ in 0..<1000 {
+                do {
+                    let response: EventLoopFuture<Output> = client.execute(operation: "test", path: "/", httpMethod: .POST, serviceConfig: config, input: input, logger: TestEnvironment.logger)
+
+                    try awsServer.processRaw { request in
+                        let receivedInput = try JSONDecoder().decode(Input.self, from: request.body)
+                        let output = Output(s: receivedInput.s, i: receivedInput.i.count)
+                        let byteBuffer = try JSONEncoder().encodeAsByteBuffer(output, allocator: ByteBufferAllocator())
+                        let response = AWSTestServer.Response(httpStatus: .ok, headers: [:], body: byteBuffer)
+                        return .result(response)
+                    }
+
+                    let output = try response.wait()
+                } catch {
+                    
+                }
+            }
+            return
+        }
+    }
 
     func testClientWithInputNoOutput() {
         enum InputEnum: String, Codable {
